a07f560ea3a9d4b3f3b664a47afbb662ce20442e
nukopy <pytwbf201830@gmail.com>
2020-11-05 10:36:19 +0900

    feat: add base class of AWS client

---
+++ data-infrastructure/app/aws/base.py
@@ -0,0 +1,25 @@
+import os
+import boto3
+
+
+class Base:
+    def __init__(self, service: str):
+        self.access_key = os.getenv("AWS_ACCESS_KEY_ID")
+        self.secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+        self.region = os.getenv("AWS_REGION")
+
+        # client
+        if service == "firehose":
+            self.client = boto3.client(
+                service,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key,
+                region_name=self.region,
+            )
+        else:
+            self.client = boto3.resource(
+                service,
+                aws_access_key_id=self.access_key,
+                aws_secret_access_key=self.secret_key,
+                region_name=self.region,
+            )
