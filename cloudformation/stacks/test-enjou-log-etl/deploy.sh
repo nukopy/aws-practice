c1e72860458fcb808a9747616a78464438356071
nukopy <pytwbf201830@gmail.com>
2020-11-13 11:54:22 +0900

    update: deploy script

--- cloudformation/stacks/test-enjou-log-etl/deploy.sh
+++ cloudformation/stacks/test-enjou-log-etl/deploy.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-CHANGESET_OPTION="--no-execute-changeset"
+# variables
+Prefix="test-enjou-cfn"
+BUCKET_NAME="${Prefix}-lambda-deploy-packages"
+CFN_TEMPLATE="./template.yml"
+CFN_TEMPLATE_PACKAGED="./template-packaged.yml"
+CFN_STACK_NAME="test-lambda-cwl"
+CHANGESET_OPTION=""
+CFN_TEMPLATE="./template/template-packaged.yml"
+CFN_STACK_NAME="test-enjou-log-etl"
 
+# upload Lambda deploy packages to S3
+aws cloudformation package \
+  --template-file "${CFN_TEMPLATE}" \
+  --s3-bucket "${BUCKET_NAME}" \
+  --output-template-file ${CFN_TEMPLATE_PACKAGED}
+
+# deploy
+CHANGESET_OPTION="--no-execute-changeset"
 if [ $# = 1 ] && [ $1 = "deploy" ]; then
   echo "deploy mode"
   CHANGESET_OPTION=""
 fi
 
-CFN_TEMPLATE="./template/template-packaged.yml"
-CFN_STACK_NAME="test-enjou-log-etl"
-
-# deploy
-Prefix="test-enjou-cfn"
-
-# IAM に関するリソースを作るときにこのオプションを扶養する必要がある（cf: https://github.com/aws/serverless-application-model/issues/51）
+# IAM に関するリソースを作るときに --capabilities オプションを扶養する必要がある（cf: https://github.com/aws/serverless-application-model/issues/51）
 aws cloudformation deploy ${CHANGESET_OPTION} \
-  --template "${CFN_TEMPLATE}" \
+  --template "${CFN_TEMPLATE_PACKAGED}" \
   --stack-name "${CFN_STACK_NAME}" \
   --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
   --parameter-overrides \
--- cloudformation/stacks/test-enjou-log-etl/upload-lambda-deploy-packages.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-Prefix="test-enjou-cfn"
-BUCKET_NAME="${Prefix}-lambda-deploy-packages"
-
-aws cloudformation package \
-  --template-file template.yml \
-  --s3-bucket "${BUCKET_NAME}" \
-  --output-template-file template-packaged.yml
