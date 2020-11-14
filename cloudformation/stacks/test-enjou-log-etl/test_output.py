5ee09221d31f42b03a7e2b2d97abf6549d508b43
nukopy <pytwbf201830@gmail.com>
2020-11-14 19:12:35 +0900

    test: add test for Avro transformation

--- cloudformation/stacks/test-enjou-log-etl/test_output.py
+++ cloudformation/stacks/test-enjou-log-etl/test_output.py
@@ -1,1 +1,34 @@
-# TODO: Avro 形式のファイルがちゃんと確認できるか
+import json
+from typing import Any, Dict, List
+
+import fastavro
+
+
+def read_json_lines(filepath: str) -> List[Dict[str, Any]]:
+    with open(file=filepath, mode="r", encoding="utf-8") as fp_input:
+        json_lines: List[str] = fp_input.readlines()
+        json_lines: List[Dict[str, Any]] = [json.loads(line) for line in json_lines]
+
+    return json_lines
+
+
+def read_avro(filepath: str) -> List[Dict[str, Any]]:
+    with open(file=filepath, mode="rb") as f:
+        records = []
+        for record in fastavro.reader(f):
+            records.append(record)
+
+    return records
+
+
+def test_reading_avro():
+    dir_output = "./output/"
+    path_json_line = dir_output + "log"
+    path_avro = dir_output + "log.avro"
+
+    # read
+    records_json_line = read_json_lines(filepath=path_json_line)
+    records_avro = read_avro(filepath=path_avro)
+
+    # test
+    assert records_json_line == records_avro
